bb3f4b1f77d8f742a6c159167af31bc9ecceab3f
lagbugbot <35502831+lagbugbot@users.noreply.github.com>
2018-01-25 00:09:24 +0600

    Update class+lugbot.py

--- class+lugbot.py
+++ class+lugbot.py
@@ -67,16 +67,33 @@ def main():
             last_update_id = last_update['update_id']
             last_chat_text = last_update['message']['text']
             last_chat_id = last_update['message']['chat']['id']
-            last_chat_name = last_update['message']['chat']['first_name']       
+            last_chat_name = last_update['message']['from']['first_name']
+            print (last_chat_text)
+            print (last_chat_id)
+            print (last_chat_name)
         except Exception as e:
             pass
             print(e)
-        if last_chat_text.lower() in badwords:
-            greet_bot.send_message(last_chat_id, 'сам ты {}'.format(last_chat_text))
-        elif last_chat_text.lower() == 'боря хуй':
-            greet_bot.send_message(last_chat_id, 'сам ты, Сережа, хуй')
-        else:
-            greet_bot.send_message(last_chat_id, last_chat_text)
+        
+        if last_chat_text == 'У':
+            greet_bot.send_photo(last_chat_id, 'https://pp.userapi.com/c540108/v540108844/815c/0Ei7pxV3gyE.jpg')
+        if last_chat_text == 'R':
+            greet_bot.send_photo(last_chat_id, 'https://web.telegram.org/86194106-93ac-40d4-8126-03f865f15540')
+            
+            
+#        if last_chat_name == 'PhazMinze':
+#            greet_bot.send_message(last_chat_id, 'Даур, не надо')
+#        elif last_chat_name == 'Arsen':
+#            greet_bot.send_message(last_chat_id, 'Не слушайте его')
+#        elif last_chat_name == 'Boris':
+#            greet_bot.send_message(last_chat_id, 'Он прав')
+            
+#        if last_chat_text.lower() in badwords:
+#            greet_bot.send_message(last_chat_id, 'сам ты {}'.format(last_chat_text))
+#        elif last_chat_text.lower() == 'боря хуй':
+#            greet_bot.send_message(last_chat_id, 'сам ты, Сережа, хуй')
+#        else:
+#           greet_bot.send_message(last_chat_id, last_chat_text)
 
             
         new_offset = last_update_id + 1
